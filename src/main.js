60163377b1bd4dbc171f184f8a9d8804d41340d4
= <2314736996@qq.com>
2020-09-26 10:01:51 +0800

    第二次提交

---
+++ src/main.js
@@ -0,0 +1,70 @@
+import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+// import enLang from 'element-ui/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
+
+import * as filters from './filters' // global filters
+
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  // locale: enLang // 如果使用中文，无需设置，请删除
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+})
+
+//权限拦截器
+router.beforeEach((to, from, next) => {
+  console.log('当前访问路径：'+to.path)
+    console.log("1")
+    console.log('客服管理员' ===store.state.title)
+    if (store.state.title === '客服管理员') {
+      if(to.path === '/boorrowing/chart_month' || to.path === '/boorrowing/chart_day'
+        || to.path === '/user_management/user' || to.path === '/charts/mix-chart'
+        || to.path === '/financial_product/mix-chart' || to.path === '/financial_product/product'
+        || to.path === '/large_loans/audit_loans' || to.path === '/large_loans/complex-table'
+        || to.path === '/large_loans/mix-chart' || to.path === '/financial/financial_index'){
+        alert("你不是系统管理员！")
+        next('/');
+      }else{
+        next();
+      }
+    }else{
+      next();
+    }
+})
---
+++ src/views/customer/historical_chat_record.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+      <div id="demo">
+        <a id="a">历史记录</a>
+        <el-button id="ssk2" type="primary" icon="el-icon-search"></el-button>
+        <input  id="ssk" v-model="input" placeholder="   请输入内容" ></input>
+
+        <!--<h2>历史记录</h2>-->
+      </div>
+
+      <div>
+      <el-radio-group v-model="tabPosition" style="margin-bottom: 50px;">
+        <!--<el-radio-button label="top">top</el-radio-button>-->
+        <!--<el-radio-button label="right">right</el-radio-button>-->
+        <!--<el-radio-button label="bottom">bottom</el-radio-button>-->
+        <!--<el-radio-button label="left">left</el-radio-button>-->
+      </el-radio-group>
+
+      <el-tabs :tab-position="tabPosition" style="height:100%;">
+        <el-tab-pane label="全部" >
+          <div>暂无历史记录</div>
+        </el-tab-pane>
+        <el-tab-pane label="今日" >
+          <div>暂无历史记录</div>
+
+        </el-tab-pane>
+        <el-tab-pane label="昨日">暂无历史记录</el-tab-pane>
+        <el-tab-pane label="过去7天">暂无历史记录</el-tab-pane>
+        <el-tab-pane label="本月">暂无历史记录</el-tab-pane>
+        <el-tab-pane label="八月">暂无历史记录</el-tab-pane>
+        <el-tab-pane label="七月">暂无历史记录</el-tab-pane>
+        <el-tab-pane label="六月">暂无历史记录</el-tab-pane>
+      </el-tabs>
+      </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tabPosition: 'left',
+        input: ''
+      };
+    }
+  };
+</script>
+
+<style scoped>
+    #demo{
+      width: 100%;
+      height: 60px;
+      background-color: #5a5e66;
+    }
+    #a{
+      color: #fbfdff;
+      font-size: 30px;
+      position: absolute;
+      left: 30px;
+      top: 10px;
+    }
+  #ssk{
+    color: #000000;
+    width: 600px;
+    height: 40px;
+    position: absolute;
+    left: 540px;
+    top: 10px;
+    background-color: #1890ff ;
+    outline: 0;
+    border-radius: 10px;
+    border: 0px  solid;
+
+  }
+    #ssk2{
+      font-size: 18px;
+      position: absolute;
+      left: 500px;
+      top: 10px;
+    }
+
+    ::-webkit-input-placeholder{
+      color:#fbfdff;
+      font-size:16px;
+    }
+</style>
